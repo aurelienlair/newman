2f95caec5131511419085ec6e3a61b95bedf0c98
Aurélien Lair <aurelien.lair@musement.com>
2020-12-23 17:56:46 +0100

    style: indent js code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const newman = require('/usr/local/share/npm-global/lib/node_modules/newman');
 
 try {
-newman.run({
-  collection: './src/config/collection.json',
-  reporters: 'cli',
-  bail: true
+  newman.run({
+    collection: './src/config/collection.json',
+    reporters: 'cli',
+    bail: true
 });
 } catch (error) {
   console.log(error);
